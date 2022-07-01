f990b8c17ce58c9d4edd9b800391d937f22c7503
ZaniInc <stridestoreholding@gmail.com>
2022-07-01 10:58:42 +0300

    "task3"

--- interfaces/IVesting.sol
+++ interfaces/IVesting.sol
@@ -3,17 +3,16 @@
 pragma solidity 0.8.7;
 
 interface IVesting {
-
-    event VestingStart (uint256 startDate);
-
-    event AddInvestors (address[] investors , uint256[] balances);
-
-    event Withdraw (address to, uint256 amountTokens);
-
     enum Allocation {
         Seed,
         Private
     }
+
+    event VestingStart(uint256 startDate);
+
+    event AddInvestors(address[] investors, uint256[] balances);
+
+    event Withdraw(address to, uint256 amountTokens);
 
     function setInitialTimestamp(uint256 initialTimestamp_) external;
 
